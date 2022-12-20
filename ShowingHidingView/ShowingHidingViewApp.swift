4dcfbc36ef536a0c214aed4e52b43b83fdfde12f
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-20 17:42:49 -0300

    Initial Commit

---
+++ ShowingHidingView/ShowingHidingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowingHidingViewApp.swift
+//  ShowingHidingView
+//
+//  Created by Orlando Moraes Martins on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ShowingHidingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
