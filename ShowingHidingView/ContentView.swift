d9a0fe842a3943f917d200f5185912f8b2648304
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-20 17:46:04 -0300

    Showing and hiding a view

--- ShowingHidingView/ContentView.swift
+++ ShowingHidingView/ContentView.swift
@@ -8,17 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowingRed = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            Button("Tap me"){
+                withAnimation{
+                    isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
         }
-        .padding()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
